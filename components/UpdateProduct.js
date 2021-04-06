51582e8369832e58eb42d5fa2915d3ab44fb45d2
Paul Morris <paul@cratebind.com>
2021-04-06 16:34:42 -0500

    Initial commit

---
+++ components/UpdateProduct.js
@@ -0,0 +1,110 @@
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import { Router, useRouter } from 'next/router';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  const router = useRouter();
+  // 1. Need to get existing product
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+  // 2. Need to get mutation to update product
+  const [
+    updateProduct,
+    { data: updateData, error: updateError, loading: updateLoading },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION);
+  // 2.5 Create some state for the form inputs
+  const { inputs, handleChange, resetForm, clearForm } = useForm(data?.Product);
+  if (loading) return <p>Loading...</p>;
+  // 3. Need form to handle updates
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const res = await updateProduct({
+          variables: {
+            id,
+            name: inputs.name,
+            description: inputs.description,
+            price: inputs.price,
+          },
+        }).catch(console.error);
+        // Submit the input fields to the backend
+        router.push({
+          pathname: `/product/${res.data.updateProduct.id}`,
+        });
+      }}
+    >
+      <DisplayError error={error || updateError} />
+      <fieldset disabled={updateLoading} aria-busy={loading}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Update Product</button>
+      </fieldset>
+    </Form>
+  );
+}
---
+++ components/styles/CloseButton.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const CloseButton = styled.button`
+  background: black;
+  cursor: pointer;
+  color: white;
+  font-size: 3rem;
+  border: 0;
+  position: absolute;
+  z-index: 2;
+  right: 0;
+`;
+
+export default CloseButton;
